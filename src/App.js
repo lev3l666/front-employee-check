9a558140eb9b91b08989f462a981e7b209eb74f9
Jairo Sierra <jairo@tvsgroup.nl>
2021-06-10 23:53:24 -0500

    Table of employees

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import { DataGrid } from '@material-ui/data-grid';
 import { EmployeeList } from "./components/grid.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <EmployeeList></EmployeeList>
-      </header>
+      
+      <EmployeeList></EmployeeList>
     </div>
   );
 }
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import {get, post} from '../services/api';
 import { employees } from "../services/endpoint.json";
+import { DataGrid } from '@material-ui/data-grid';
 
-let response = async () => { return await get(employees) };
 export class EmployeeList extends React.Component {
 constructor() {
     super();
@@ -15,6 +15,7 @@ constructor() {
         console.log('antes');
         await get(employees).then(res => {
             console.log('res: ',res.data.message);
+            console.log('Keys: ',Object.values(res.data.message));
             this.setState({users : res.data.message })
         });
     }
@@ -22,14 +23,31 @@ constructor() {
   render() {
     return (
         <div>
-            <ul>
+            {/* <ul>
             {this.state.users.map(user => <li>{user.name1} mail: {user.mail}</li>)}
-            </ul>
+            </ul> */}
+            <div style={{ height: 400, width: '100%' }}>
+      <DataGrid rows={this.state.users} columns={columns} />
+    </div>
         </div>
     )
   }
 }
 
-
+const columns = [
+    { field: 'name1', headerName: 'First name', editable: true, width: 150 },
+    { field: 'name2', headerName: 'Second name', editable: true  , width: 150},
+    { field: 'lastName1', headerName: 'First lastname', editable: true  , width: 150},
+    { field: 'lastName2', headerName: 'Second lastname', editable: true  , width: 150},
+    { field: 'country', headerName: 'Country', editable: true  , width: 150}, // list
+    { field: 'area', headerName: 'Area', editable: true  , width: 150}, // list
+    { field: 'idType', headerName: 'ID Type', editable: true  , width: 150}, // list
+    { field: 'employeeId', headerName: 'Employee ID', editable: true  , width: 150}, // no se si editable
+    { field: 'mail', headerName: 'Email', editable: false  , width: 150}, 
+    { field: 'admission', headerName: 'Admission', type: 'dateTime', editable: true, width: 150},
+    { field: 'status', headerName: 'Status', editable: false  , width: 150}, 
+    { field: 'createdAt', headerName: 'createdAt', type: 'dateTime', editable: false, width: 150},
+    { field: 'updatedAt', headerName: 'updatedAt', type: 'dateTime', editable: false, width: 150},
+  ];
 
 export default EmployeeList;
