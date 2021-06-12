262b19e9927d060e0f21e958223fb5df4354126a
Jairo Sierra <jairo@tvsgroup.nl>
2021-06-11 19:51:46 -0500

    Improved UI

---
+++ src/components/default.jsx
@@ -0,0 +1,125 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import Paper from '@material-ui/core/Paper';
+import Fab from '@material-ui/core/Fab';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import Avatar from '@material-ui/core/Avatar';
+import MenuIcon from '@material-ui/icons/Menu';
+import AddIcon from '@material-ui/icons/Add';
+import SearchIcon from '@material-ui/icons/Search';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import EmployeeList from "./grid.jsx";
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+
+
+const useStyles = makeStyles((theme) => ({
+  text: {
+    padding: theme.spacing(2, 2, 0),
+  },
+  paper: {
+    paddingBottom: 50,
+  },
+  list: {
+    marginBottom: theme.spacing(2),
+  },
+  subheader: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  fabButton: {
+    position: 'absolute',
+    zIndex: 1,
+    top: -30,
+    left: 0,
+    right: 0,
+    margin: '0 auto',
+  },
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
+export function BottomAppBar() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => {
+    setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Paper square className={classes.paper}>
+        <Typography className={classes.text} variant="h5" gutterBottom>
+          Employee list
+        </Typography>
+        <EmployeeList></EmployeeList>
+      </Paper>
+      <AppBar position="fixed" color="primary" className={classes.appBar}>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+            <AddIcon onClick={handleOpen}/>
+          </Fab>
+          <div className={classes.grow} />
+          <IconButton edge="end" color="inherit">
+            <MoreIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <h2 id="transition-modal-title">Transition modal</h2>
+            <p id="transition-modal-description">react-transition-group animates me.</p>
+          </div>
+        </Fade>
+      </Modal>
+    </React.Fragment>
+  );
+}
+
+export default BottomAppBar;
--- src/components/grid.jsx
+++ src/components/grid.jsx
@@ -26,7 +26,7 @@ constructor() {
             {/* <ul>
             {this.state.users.map(user => <li>{user.name1} mail: {user.mail}</li>)}
             </ul> */}
-            <div style={{ height: 400, width: '100%' }}>
+            <div style={{ height: '85vh', width: '100%' }}>
       <DataGrid rows={this.state.users} columns={columns} />
     </div>
         </div>
@@ -35,6 +35,7 @@ constructor() {
 }
 
 const columns = [
+    // { field: 'id', headerName: 'ID', editable: false, width: 150 }, disabled
     { field: 'name1', headerName: 'First name', editable: true, width: 150 },
     { field: 'name2', headerName: 'Second name', editable: true  , width: 150},
     { field: 'lastName1', headerName: 'First lastname', editable: true  , width: 150},
@@ -45,7 +46,7 @@ const columns = [
     { field: 'employeeId', headerName: 'Employee ID', editable: true  , width: 150}, // no se si editable
     { field: 'mail', headerName: 'Email', editable: false  , width: 150}, 
     { field: 'admission', headerName: 'Admission', type: 'dateTime', editable: true, width: 150},
-    { field: 'status', headerName: 'Status', editable: false  , width: 150}, 
+    // { field: 'status', headerName: 'Status', editable: false  , width: 150}, disabled
     { field: 'createdAt', headerName: 'createdAt', type: 'dateTime', editable: false, width: 150},
     { field: 'updatedAt', headerName: 'updatedAt', type: 'dateTime', editable: false, width: 150},
   ];
